1794bb0b4c0ef22e90dd3e76f9274c3d64bee32d
Johan Burell <johan.burell@valtech.com>
2021-12-01 22:28:00 +0100

    Solution part 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::{VecDeque};
+use std::collections::vec_deque::Iter;
+
 pub fn count_number_of_increases(numbers: Vec<u32>) -> usize {
   let (count, _) = numbers
     .iter()
@@ -8,6 +11,34 @@ pub fn count_number_of_increases(numbers: Vec<u32>) -> usize {
           (count, *elem)
         }
       }
+    );
+  count
+}
+
+type Elem = Option<u32>;
+
+pub fn count_number_of_increases_in_sliding_window(numbers: Vec<u32>) -> usize {
+  let rotate_ring_buffer = 
+    |buf: &mut VecDeque<_>, value| {buf.push_front(Some(value)); buf.pop_back()};
+  let count_value_of_buf = 
+    |buf: Iter<Elem>| buf.fold(0, |a, v| v.map_or(a, |n| a + n));
+  
+  let (count, _) = numbers
+    .iter()
+    .fold((0, VecDeque::from([None, None, None])),|(count, mut last), elem| {
+      if last.iter().any(|v| *v == None) {
+        rotate_ring_buffer(&mut last, *elem);
+        (count, last)
+      } else {
+        if count_value_of_buf(last.iter()) < count_value_of_buf(last.range(0..2)) + *elem {
+          rotate_ring_buffer(&mut last, *elem);
+          (count + 1, last)
+        } else {
+          rotate_ring_buffer(&mut last, *elem);
+          (count, last)
+        }
+      }
+    }
     );
   count
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,21 +1,37 @@
 use std::fs;
-use AdventOfCode21_Day1::count_number_of_increases;
+use advent_of_code21_day1::*;
+
+fn get_data(filename: &str) -> Vec<u32> {
+  fs::read_to_string(filename).unwrap()
+    .lines()
+    .map(|s| s.trim().to_owned())
+    .filter(|s| !s.is_empty())
+    .map(|s| u32::from_str_radix(&s,10).unwrap())
+    .collect()
+}
 
 #[test]
 fn part_1_count_number_of_increases_in_data_series() {
-    // Arrange
-    let values: Vec<u32> =
-        fs::read_to_string("input.txt").unwrap()
-            .lines()
-            .map(|s| s.trim().to_owned())
-            .filter(|s| !s.is_empty())
-            .map(|s| u32::from_str_radix(&s,10).unwrap())
-            .collect();
-    
-    // Act
-    let actual = count_number_of_increases(values);
-    
-    // Assert
-    assert_eq!(1688, actual);
-    println!("Answer 1: {}", actual)
+  // Arrange
+  let values: Vec<u32> = get_data("input.txt");
+  
+  // Act
+  let actual = count_number_of_increases(values);
+
+  // Assert
+  assert_eq!(1688, actual);
+  println!("Answer 1: {}", actual)
+}
+
+#[test]
+fn part_2_count_number_of_increases_in_sliding_window() {
+  // Arrange
+  let values: Vec<u32> = get_data("input.txt");
+
+  // Act
+  let actual = count_number_of_increases_in_sliding_window(values);
+
+  // Assert
+  assert_eq!(1728, actual);
+  println!("Answer 2: {}", actual)
 }   
